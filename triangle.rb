49d5fa482d6c2409d3e145af57d5f5b411776023
Ryan Kerr <ryandanielkerr@gmail.com>
2020-09-15 15:08:59 -0700

    Finished koans

---
+++ triangle.rb
@@ -0,0 +1,43 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  # WRITE THIS CODE
+  sides = [a, b, c]
+  sides.sort!
+  if sides[2] >= sides[0] + sides[1]
+    raise TriangleError, "Invalid Side Lengths"
+  end
+  
+  for side in sides
+    if side <= 0
+      raise TriangleError, "Invalid Side Lengths"
+    end
+  end
+
+  unique_side_lengths = sides.uniq.size
+  case unique_side_lengths
+  when 1
+    :equilateral
+  when 2
+    :isosceles
+  when 3
+    :scalene
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
